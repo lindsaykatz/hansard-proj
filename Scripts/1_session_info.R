11a862b7ebc27a82e6f0cec5d54b0b9752251e8b
Lindsay Katz <lindsaykatz16@gmail.com>
2022-07-25 11:40:26 -0400

    updated script

--- Scripts/1_session_info.R
+++ Scripts/1_session_info.R
@@ -4,7 +4,7 @@ library(XML)
 library(here)
 
 # parse XML
-doc <- xmlParse(here("hansard-proj/XML_files", "House of Representatives_2021_11_30_Official.xml"))
+doc <- xmlParse(here("hansard-proj/XML_files", "2021_11_30.xml"))
 
 # store session information in data frame
 session_info <- xmlToDataFrame(node=getNodeSet(doc, "//session.header"))
